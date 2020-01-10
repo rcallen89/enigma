e79a1b95ccef9170967bb64a0b382ba58a69c78c
Ryan Allen <rcallen89@gmail.com>
2020-01-10 08:44:21 -0700

    Create the enigma class and test

---
+++ test/enigma_test.rb
@@ -0,0 +1,14 @@
+require_relative './test_helper'
+require_relative '../lib/enigma'
+require 'mocha/minitest'
+
+class EnigmaTest < Minitest::Test
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_enigma_exists
+    assert_instance_of Enigma, @enigma
+  end
+
+end
