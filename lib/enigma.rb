2175cc7d34797b28a1159203d288085b76bcc146
Ryan Allen <rcallen89@gmail.com>
2020-01-11 12:12:02 -0700

    Setup decrypt shell method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -10,17 +10,27 @@ class Enigma
     date: date}
   end
 
+  def encrypt(message, key = Array.new(5){rand(10)}.join, date = DateTime.now.strftime('%d%m%y'))
+    {encryption: joiner(message_shifter(message, key, date)),
+    key: key,
+    date: date}
+  end
+
   def message_breakdown(message)
     char_slices = []
     message.downcase.chars.each_slice(4) {|mess_break| char_slices << mess_break}
     char_slices
   end
 
-  def message_shifter(message, key, date)
+  def message_shifter(message, key, date, flag = "e")
     shift_info = Shift.new(key, date)
     broken_message = message_breakdown(message)
     broken_message.map.with_index do |char_slice, index|
-      letter_shifter(char_slice, shift_info.shifts_table.values)
+      if flag == "e"
+        letter_shifter(char_slice, shift_info.shifts_table.values)
+      else
+        letter_unshifter(char_slice, shift_info.shifts_table.values)
+      end
     end
   end
 
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -116,7 +116,7 @@ class EnigmaTest < Minitest::Test
                   date: "040895"
                 }
 
-    assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+    assert_equal expected, @enigma.decrypt("keder ohulw", "02715", "040895")
   end
 
 end
